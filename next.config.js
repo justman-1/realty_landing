b66f1cd05c54c4f4c86fab96402230f8443b09fb
justman-1 <sharom573@gmail.com>
2023-03-23 23:06:34 +0300

    1

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-/** @type {import('next').NextConfig} */
+const path = require("node:path")
+
 const nextConfig = {
-  reactStrictMode: true,
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@styles": path.resolve(__dirname, "styles"),
+    }
+
+    return config
+  },
 }
 
 module.exports = nextConfig
